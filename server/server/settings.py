9db3a3eca17514e948e0face1d69863fea3db360
antonkurenkov <a.k.a1306@gmail.com>
2021-05-30 16:48:21 +0300

    fix: allowed hosts

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-81w+e51#prunypwctfdr5#6gpy&f$(+h=2s-54!#mtmbvv=&nz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
